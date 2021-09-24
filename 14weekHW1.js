81ae0f554d9b8cb88286b4f7711b75d22794ef0a
caeruleu <84741982+caeruleu@users.noreply.github.com>
2021-09-24 21:31:41 +0300

    Update 14weekHW1.js

--- 14weekHW1.js
+++ 14weekHW1.js
@@ -1,27 +1,27 @@
 let plus = (n1, n2) => {
-    num1 = document.getElementById('n1').value;
-    num2 = document.getElementById('n2').value;
+    num1 = document.getElementById('number1').value;
+    num2 = document.getElementById('number2').value;
 
     out.innerHTML = +num1 + +num2;
 }
 
 let minus = (n1, n2) => {
-    num1 = document.getElementById('n1').value;
-    num2 = document.getElementById('n2').value;
+    num1 = document.getElementById('number1').value;
+    num2 = document.getElementById('number2').value;
 
     out.innerHTML = +num1 - +num2;
 }
 
 let times = (n1, n2) => {
-    num1 = document.getElementById('n1').value;
-    num2 = document.getElementById('n2').value;
+    num1 = document.getElementById('number1').value;
+    num2 = document.getElementById('number2').value;
 
     out.innerHTML = +num1 * +num2;
 }
 
 let divide = (n1, n2) => {
-    num1 = document.getElementById('n1').value;
-    num2 = document.getElementById('n2').value;
+    num1 = document.getElementById('number1').value;
+    num2 = document.getElementById('number2').value;
 
     out.innerHTML = +num1 / +num2;
 }
