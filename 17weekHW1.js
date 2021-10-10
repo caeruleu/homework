cba6f4129061a064ce8ae6faa3c562b7945ff7b1
caeruleu <84741982+caeruleu@users.noreply.github.com>
2021-10-10 21:37:37 +0300

    Update 17weekHW1.js

--- 17weekHW1.js
+++ 17weekHW1.js
@@ -13,16 +13,11 @@ function validateName() {
     let Name = names[1];
     let lastname = names[2];
 
-    let clearName = Name.trim(Name);
-    let validClearName = clearName[0].toUpperCase() + clearName.toLowerCase().slice(1);
+    let validSurname = surname[0].toUpperCase() + surname.toLowerCase().slice(1);
+    let validName = Name[0].toUpperCase() + Name.toLowerCase().slice(1);
+    let validLastname = lastname[0].toUpperCase() + lastname.toLowerCase().slice(1);
 
-    let clearSurname = surname.trim(surname);
-    let validClearSurname = clearSurname[0].toUpperCase() + clearSurname.toLowerCase().slice(1);
-
-    let clearLastname = lastname.trim(lastname);
-    let validClearLastname = clearLastname[0].toUpperCase() + clearLastname.toLowerCase().slice(1);
-
-    console.log("Имя: "+ validClearName);
-    console.log("Фамилия: "+ validClearSurname);
-    console.log("Отчество: "+ validClearLastname);
+    console.log("Имя: "+ validName);
+    console.log("Фамилия: "+ validSurname);
+    console.log("Отчество: "+ validLastname);
 }
