631c911383f187a66708ff4d99762edfbeb0bf00
caeruleu <84741982+caeruleu@users.noreply.github.com>
2021-11-06 22:20:38 +0300

    Update 18weekHW4.js

--- 18week/18weekHW4.js
+++ 18week/18weekHW4.js
@@ -1,3 +1,47 @@
+// вторая версия с вводом заметок через инпут:
+
+document.addEventListener('DOMContentLoaded', function() {
+        JSON.parse(localStorage.getItem("savedTasks"));
+        showTasks();
+}) 
+
+let tasks;
+
+if (localStorage.getItem("savedTasks") == null) {
+        tasks = [];
+    } else {
+        tasks = JSON.parse(localStorage.getItem("savedTasks"));
+    }
+
+
+let getTasks = () => {
+    let task = document.getElementById("forTask").value;
+    tasks.push(task);
+    document.getElementById("forTask").value = "";
+    saveTasks();
+    showTasks();
+    return tasks;
+}
+
+let saveTasks = () => {
+    localStorage.setItem("savedTasks", JSON.stringify(tasks));
+    notes = JSON.parse(localStorage.getItem("savedTasks"));
+}
+
+let showTasks = () => {
+    let optionString = "";
+    for (let task of tasks) {
+        optionString += `<div><input type='checkbox'><span>${task}</span></div>`;
+    }
+    document.getElementById("existingNotes").innerHTML = optionString;
+}
+
+let clearTasks = () => {
+    localStorage.clear();
+}
+
+// изначальная версия с prompt:
+
 let getTasks = () => {
 let tasks = [];
         for (;;) {
