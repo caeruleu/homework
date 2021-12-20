03909c21c1c10a56dc3b54968a6969140e578438
caeruleu <84741982+caeruleu@users.noreply.github.com>
2021-12-20 20:45:02 +0300

    Add files via upload

---
+++ cards-app/src/footer.jsx
@@ -0,0 +1,9 @@
+import styles from "./footer.css";
+
+function Footer() {
+    return(
+        <div className="footer"></div>
+    );
+}
+
+export default Footer;
---
+++ cards-app/src/header.jsx
@@ -0,0 +1,9 @@
+import styles from './header.css';
+
+function Header() {
+    return(
+        <div className='header'></div>
+    );
+}
+
+export default Header;
