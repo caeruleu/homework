5e21bd8f72de3a2ffe0ae423ea965e3ddadab4f5
caeruleu <84741982+caeruleu@users.noreply.github.com>
2022-02-09 17:07:48 +0300

    Delete cards-app directory

--- cards-app/src/footer.jsx
+++
@@ -1,9 +0,0 @@
-import styles from "./footer.css";
-
-function Footer() {
-    return(
-        <div className={styles.Footer}></div>
-    );
-}
-
-export default Footer;
--- cards-app/src/header.jsx
+++
@@ -1,9 +0,0 @@
-import styles from './header.css';
-
-function Header() {
-    return(
-        <div className='header'></div>
-    );
-}
-
-export default Header;
